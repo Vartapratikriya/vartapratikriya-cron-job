d4a3d0a836648413297ab7c4614286f28919004b
Rajdeep Ghosh <007rajdeepghosh@gmail.com>
2023-09-20 20:58:34 +0530

    Update cron_job.py

--- src/cron_job.py
+++ src/cron_job.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from news_listener import load_config, NewsListener
 from keyword_extractor import KeywordExtractor
 from sentiment_analyser import SentimentAnalyser
-from fake_news_analyser import FactChecker
+from fact_checker import FactChecker
 
 
 class CronJob:
@@ -81,22 +81,20 @@ class CronJob:
     def run(self) -> None:
         headlines = self.listener.get_headlines()
         categorised = self.listener.get_categorised()
-        # headlines_dict = self.generate_sentiment(data=headlines, type="headlines")
-        # categorised_dict = self.generate_sentiment(data=categorised, type="categorised")
-        # top_dict = self.generate_keywords(data=headlines_dict)
+        headlines_dict = self.generate_sentiment(data=headlines, type="headlines")
+        categorised_dict = self.generate_sentiment(data=categorised, type="categorised")
+        top_dict = self.generate_keywords(data=headlines_dict)
         factChecker_dict = self.generate_fact(data=categorised, type="headlines")
 
         print(factChecker_dict)
 
-        # self.push_db(docs=top_dict, collection_name="top_keywords", type="one")
-        # self.push_db(docs=headlines_dict, collection_name="headlines", type="many")
-        # self.push_db(docs=categorised_dict, collection_name="categorised", type="many")
-        # self.push_db(docs=factChecker_dict, collection_name="categorised", type="many")
+        self.push_db(docs=top_dict, collection_name="top_keywords", type="one")
+        self.push_db(docs=headlines_dict, collection_name="headlines", type="many")
+        self.push_db(docs=categorised_dict, collection_name="categorised", type="many")
+        self.push_db(docs=factChecker_dict, collection_name="categorised", type="many")
 
 
 if __name__ == "__main__":
     load_dotenv()
-    os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv(
-        "HUGGINGFACE_HUB_ACCESS_TOKEN")
     job = CronJob()
     job.run()
